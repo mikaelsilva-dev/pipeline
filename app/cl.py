a3e38469c45a914eeea6831f8a23b1265589622d
mikaelvidal-dev <mikael.vidal2021@gmail.com>
2025-12-11 19:51:33 -0300

    feature: Start project for study CI/CD

---
+++ app/cl.py
@@ -0,0 +1,15 @@
+class Test:
+
+    def __init__(self):
+        self.var1 = 10
+        self.var2 = 20
+        self.var3 = 'teste'
+
+    def var1(self):
+        return self.var1
+
+    def var2(self):
+        return self.var2
+
+    def var3(self):
+        return self.var3
---
+++ app/main.py
@@ -0,0 +1,9 @@
+from cl import Test
+
+
+if __name__ == "__main__":
+    print('Valores da funcionalidade')
+    c = Test()
+    print(c.var1)
+    print(c.var2)
+    print(c.var3)
